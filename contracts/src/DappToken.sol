e0b866c5003ed6c304eed5e1ad3bb21445234945
Jer-B <111217617+Jer-B@users.noreply.github.com>
2024-02-13 10:46:29 +0900

    update contracts natspec

--- contracts/src/DappToken.sol
+++ contracts/src/DappToken.sol
@@ -1,9 +1,17 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/// @title DappToken
+/// @dev This contract is for creating a custom ERC20 token.
+
 contract DappToken is ERC20 {
-    constructor() public ERC20("Dapp Token", "DAPP"){
+    /// @notice Constructor to create DappToken
+    /// @dev Calls ERC20 constructor with name and symbol, mints initial supply to deployer.
+    /// @dev Mint 1 million tokens at deployment to msg.sender
+    constructor() public ERC20("Dapp Token", "DAPP") {
         _mint(msg.sender, 1000000000000000000000000);
     }
 }
--- contracts/src/TokenFarm.sol
+++ contracts/src/TokenFarm.sol
@@ -5,21 +5,50 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "../lib/@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
+/**
+ * @title TokenFarm
+ * @dev A contract for staking tokens to earn rewards. Users can stake ERC20 tokens in
+ * the contract to earn rewards in the form of Dapp tokens. The contract allows for
+ * staking multiple types of tokens, with each token having its own price feed to
+ * calculate the value of the stake in terms of Dapp tokens.
+ */
+
 contract TokenFarm is Ownable {
+    /**
+     * @notice Tracks the amount of a specific token staked by a user.
+     */
     // mapping token address -> staker address -> amount
     mapping(address => mapping(address => uint256)) public stakingBalance;
+
+    /**
+     * @notice Tracks the number of unique tokens each user has staked.
+     */
     mapping(address => uint256) public uniqueTokensStaked;
+
+    /**
+     * @notice Maps a token to its corresponding price feed contract.
+     */
     mapping(address => address) public tokenPriceFeedMapping;
+
+    /**
+     * @notice An array of all stakers.
+     */
     address[] public stakers;
+
+    /**
+     * @notice An array of all the tokens that can be staked in this contract.
+     */
     address[] public allowedTokens;
+
+    /**
+     * @notice The token distributed as a reward for staking.
+     */
     IERC20 public dappToken;
 
-    // stakeTokens - DONE!
-    // unStakeTokens - DONE
-    // issueTokens - DONE!
-    // addAllowedTokens - DONE!
-    // getValue - DONE!
-
+    /**
+     * @dev Sets the Dapp token used for rewards.
+     * @param _dappTokenAddress The address of the Dapp token contract.
+     */
     // 100 ETH 1:1 for every 1 ETH, we give 1 DappToken
     // 50 ETH and 50 DAI staked, and we want to give a reward of 1 DAPP / 1 DAI
 
@@ -27,10 +56,18 @@ contract TokenFarm is Ownable {
         dappToken = IERC20(_dappTokenAddress);
     }
 
+    /**
+     * @dev Allows the owner to set the price feed contract for a token.
+     * @param _token The address of the token.
+     * @param _priceFeed The address of the token's price feed contract.
+     */
     function setPriceFeedContract(address _token, address _priceFeed) public onlyOwner {
         tokenPriceFeedMapping[_token] = _priceFeed;
     }
 
+    /**
+     * @dev Issues rewards to all stakers based on their total value staked.
+     */
     function issueTokens() public onlyOwner {
         // Issue tokens to all stakers
         for (uint256 stakersIndex = 0; stakersIndex < stakers.length; stakersIndex++) {
@@ -40,6 +77,11 @@ contract TokenFarm is Ownable {
         }
     }
 
+    /**
+     * @dev Calculates the total value staked by a user across all tokens.
+     * @param _user The address of the user.
+     * @return The total value staked by the user.
+     */
     function getUserTotalValue(address _user) public view returns (uint256) {
         uint256 totalValue = 0;
         require(uniqueTokensStaked[_user] > 0, "No tokens staked!");
@@ -55,6 +97,12 @@ contract TokenFarm is Ownable {
         return totalValue;
     }
 
+    /**
+     * @dev Calculates the value staked by a user in a specific token.
+     * @param _user The address of the user.
+     * @param _token The address of the token.
+     * @return The value staked by the user in the specified token.
+     */
     function getUserSingleTokenValue(address _user, address _token) public view returns (uint256) {
         if (uniqueTokensStaked[_user] <= 0) {
             return 0;
@@ -67,6 +115,12 @@ contract TokenFarm is Ownable {
         ((stakingBalance[_token][_user] * price) / (10 ** decimals));
     }
 
+    /**
+     * @dev Retrieves the current price and decimal precision of a token from its price feed.
+     * @param _token The address of the token.
+     * @return price The current price of the token.
+     * @return decimals The decimal precision of the price.
+     */
     function getTokenValue(address _token) public view returns (uint256, uint256) {
         // priceFeedAddress
         address priceFeedAddress = tokenPriceFeedMapping[_token];
@@ -76,6 +130,11 @@ contract TokenFarm is Ownable {
         return (uint256(price), decimals);
     }
 
+    /**
+     * @dev Allows users to stake tokens in the contract.
+     * @param _amount The amount of tokens to stake.
+     * @param _token The address of the token to stake.
+     */
     function stakeTokens(uint256 _amount, address _token) public {
         require(_amount > 0, "Amount must be more than 0");
         require(tokenIsAllowed(_token), "Token is currently no allowed");
@@ -87,6 +146,10 @@ contract TokenFarm is Ownable {
         }
     }
 
+    /**
+     * @dev Allows users to unstake tokens and withdraw them from the contract.
+     * @param _token The address of the token to unstake.
+     */
     function unstakeTokens(address _token) public {
         uint256 balance = stakingBalance[_token][msg.sender];
         require(balance > 0, "Staking balance cannot be 0");
@@ -105,16 +168,30 @@ contract TokenFarm is Ownable {
         }
     }
 
+    /**
+     * @dev Updates the count of unique tokens staked by a user.
+     * @param _user The address of the user.
+     * @param _token The address of the staked token.
+     */
     function updateUniqueTokensStaked(address _user, address _token) internal {
         if (stakingBalance[_token][_user] <= 0) {
             uniqueTokensStaked[_user] = uniqueTokensStaked[_user] + 1;
         }
     }
 
+    /**
+     * @dev Allows the owner to add a token to the list of allowed tokens for staking.
+     * @param _token The address of the token to add.
+     */
     function addAllowedTokens(address _token) public onlyOwner {
         allowedTokens.push(_token);
     }
 
+    /**
+     * @dev Checks if a token is allowed to be staked in the contract.
+     * @param _token The address of the token to check.
+     * @return True if the token is allowed, false otherwise.
+     */
     function tokenIsAllowed(address _token) public view returns (bool) {
         for (
             uint256 allowedTokensIndex = 0;
